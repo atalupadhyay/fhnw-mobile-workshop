6d3d13199dffd612e829a3dae552a816474e3005
chsc <christian.gauch@zuehlke.com>
2017-05-15 17:52:30 +0200

    Added forgotten solutions

--- Android/exercises/Exercise02_Intent/app/src/main/java/ch/schoeb/exercise02_intent/MainActivity.java
+++ Android/exercises/Exercise02_Intent/app/src/main/java/ch/schoeb/exercise02_intent/MainActivity.java
@@ -1,9 +1,11 @@
 package ch.schoeb.exercise02_intent;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // TODO: Find the button and add a click listener
-        // TODO: In the OnClickListener create a new Intent which targets your DetailActivity
-        // TODO: Add custom data (Extras) to your intent
-        // TODO: Start DetailActivity and show passed data in a textView on the DetailActivity
+        Button button = (Button) findViewById(R.id.buttonNavigate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,TargetActivity.class);
+                intent.putExtra(TargetActivity.EXTRA_KEY,"Test");
+                startActivity(intent);
+            }
+        });
     }
 }
--- Android/exercises/Exercise02_Intent/app/src/main/java/ch/schoeb/exercise02_intent/TargetActivity.java
+++ Android/exercises/Exercise02_Intent/app/src/main/java/ch/schoeb/exercise02_intent/TargetActivity.java
@@ -1,40 +1,53 @@
 package ch.schoeb.exercise02_intent;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
 public class TargetActivity extends AppCompatActivity {
+
+    public static final String EXTRA_KEY = "MyKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target);
+        TextView view = (TextView) findViewById(R.id.textview);
+        Intent intent = getIntent();
+        if(intent != null){
+            String stringArrayExtra = intent.getStringExtra(EXTRA_KEY);
+            view.setText(stringArrayExtra);
+        }
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_target, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_settings:
+            default:
+                Toast.makeText(this,"Settings button clicked",Toast.LENGTH_SHORT).show();
+                return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
+
     }
 }
